43970c1d593542e03d4e6ff05c71443c3ee95c36
Matthew Duncan <mlduncan@iastate.edu>
2020-09-20 15:10:12 -0500

    Some stuff

---
+++ bee/bee.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const GRAVITY = 1900
+var is_dead = false
+var FLAP_VELOCITY = Vector2(0, -500)
+var should_flap = false
+var velocity = Vector2()
+
+signal die
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+	var motion = velocity * delta
+	
+	if should_flap:
+		velocity.y = FLAP_VELOCITY.y
+		should_flap = false
+		
+	var collision_info = move_and_collide(motion)
+	handle_collisions(collision_info)
+
+func _input(event):
+	if event.is_action_pressed("flap"): should_flap = true
+
+func handle_collisions(collision_info):
+	if collision_info && is_dead == false:
+		print("DEAD: ",collision_info.get_collider().get_name())
+		emit_signal("die")
---
+++ stage/main.gd
@@ -0,0 +1,51 @@
+extends Node
+
+const SCREEN_WIDTH: float = 600.0
+const SCREEN_HEIGHT: float = 800.0
+
+const SEGMENT_WIDTH = -1
+const SPEED = 250
+var score: int = 0
+
+signal score_changed(amount)
+
+func reset():
+	score = 0
+	$bee.velocity.y = 0.0
+	$bee.position = Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+	respawn_goal($goal, SCREEN_WIDTH * 2)
+	respawn_goal($goal2, SCREEN_WIDTH * 3)
+	
+	
+
+func _ready():
+	reset()
+	var gateway: Area2D = $goal/gateway
+	var gateway2: Area2D = $goal2/gateway
+	gateway.connect("body_exited", self, "increase_score")
+	gateway2.connect("body_exited", self, "increase_score")
+	
+# Move goals
+func _process(delta):
+	$goal.position.x -= SPEED * delta
+	if $goal.position.x <= -64.0: 
+		respawn_goal($goal, SCREEN_WIDTH * 2)
+		
+	$goal2.position.x -= SPEED * delta
+	if $goal2.position.x <= -64.0: 
+		respawn_goal($goal2, SCREEN_WIDTH * 2)
+	
+
+func respawn_goal(goal, x_pos):
+	goal.position.x = x_pos
+	goal.position.y = rand_range(0.0, SCREEN_HEIGHT)
+
+# Increase the score
+func increase_score(_collider):
+	score += 1
+	emit_signal("score", 1)
+	print("score: ", score)
+
+func _on_bee_die():
+	OS.delay_msec(500)
+	reset()
